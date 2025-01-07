7c07db177c3fae426999f64f8d6c42c62d04ffb8
Evan Hellmuth <evanhellmuth@Evans-MacBook-Pro.local>
2025-01-07 13:25:07 -0500

    working commit, no annotations

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,23 @@
+// @ts-nocheck
 import React from 'react'
 import { type FC } from 'react'
-
 import { Retool } from '@tryretool/custom-component-support'
+import STLViewer from './STLViewer';
 
-export const HelloWorld: FC = () => {
-  const [name, _setName] = Retool.useStateString({
-    name: 'name'
-  })
+export const fileViewerSTL: FC = () => {
+  const [stlUrl, _setStlUrl] = Retool.useStateString({
+    name: 'stlUrl',
+    label: '.stl URL'
+  });
+
+  // Example STLs
+  // https://raw.githubusercontent.com/Buildbee/example-stl/refs/heads/main/ascii-cube.stl
+  // https://raw.githubusercontent.com/mrdoob/three.js/dev/examples/models/stl/ascii/slotted_disk.stl
 
   return (
-    <div>
-      <div>Hello {name}!</div>
+    <div className="container mx-auto p-4">
+      <h3>Displaying: {stlUrl}</h3>
+      <STLViewer url={stlUrl} />
     </div>
-  )
+  );
 }
